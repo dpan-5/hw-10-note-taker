a7876c87d7eee18d3a77b39f54b6b012615b2e84
David Pan <dpan5425@gmail.com>
2020-09-30 18:36:31 -0400

    added html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+// Import path package to get the correct file path for our html
+const path = require("path");
+
+// ROUTING
+module.exports = function(app) {
+
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    })
+
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    })
+}
